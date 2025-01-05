393efd13f29bd3145d9d2768b3f17d03d5900a55
heitorfe <heitorfelix@yahoo.com.br>
2025-01-05 12:59:01 -0300

    Timer Function

---
+++ src/repo_crawler/TimerCreateCrawlerJobs/__init__.py
@@ -0,0 +1,32 @@
+import json
+import logging
+import azure.functions as func
+from azure.storage.queue import QueueClient
+import os
+import base64
+
+def main(mytimer: func.TimerRequest) -> None:
+    logging.info('TimerCreateCrawlerJobs function triggered.')
+
+    repo_name = os.getenv('REPO_NAME')
+    container = os.getenv('CONTAINER')
+    include_dirs = os.getenv('FOLDERS_TO_INCLUDE')
+
+    if not repo_name or not container or not include_dirs:
+        logging.error("Missing environment variables")
+        return
+
+    include_dirs = eval(include_dirs)
+
+    queue_client = QueueClient.from_connection_string(os.getenv("AZURE_STORAGE_CONNECTION_STRING"), "jobs")
+
+    for directory in include_dirs:
+        message = json.dumps({
+            'repo_name': repo_name,
+            'container': container,
+            'directory': directory
+        })
+        encoded_message = base64.b64encode(message.encode('utf-8')).decode('utf-8')
+        queue_client.send_message(encoded_message)
+
+    logging.info("Messages sent to queue")
